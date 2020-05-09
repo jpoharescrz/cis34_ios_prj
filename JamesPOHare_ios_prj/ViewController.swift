323e98e9fff23600fd9e791d509f45af33fe13be
jpaulohare <jpaul.ohare@gmail.com>
2020-05-08 20:28:58 -0700

    Initial Commit

---
+++ JamesPOHare_ios_prj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JamesPOHare_ios_prj
+//
+//  Created by James P OHare on 5/8/20.
+//  Copyright © 2020 Access Unlimited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
